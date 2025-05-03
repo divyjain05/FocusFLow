7abe83d287b6acbf8f7298d0d4c1f9dbd8764fe1
divyjain05 <divyjain2005@gmail.com>
2025-05-03 15:27:48 +0530

    Heading and splash screen

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar(){
+    return (
+    <nav className = 'Navbar'>
+        <h1>FocusFlow</h1>
+        <hr className = 'navbar-line'/>
+     </nav>
+    )
+}
+
+export default Navbar
+
---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SplashScreen.css';
+
+function SplashScreen() {
+  return (
+    <div className="splash-screen">
+      <h1 className="splash-title">FocusFlow</h1>
+    </div>
+  );
+}
+
+export default SplashScreen;
